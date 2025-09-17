028d984ca67d335c7540b0a51fb406e4c477e117
Nikhil Saini <nikhil.nextbyts@gmail.com>
2025-09-17 15:02:59 +0530

    add auth services

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,3 +1,5 @@
+//this file is craeted becouse the charecter is also include with env id's so we insure only string values is goes to the api. this is reduce the chance of website crashes.
+
 const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
